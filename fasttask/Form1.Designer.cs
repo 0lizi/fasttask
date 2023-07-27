3675699cbb345b9770f8e4f1026450b1b6275645
olizi <chuxia_666@163.com>
2023-07-27 15:09:32 +0800

    上传

---
+++ fasttask/Form1.Designer.cs
@@ -0,0 +1,267 @@
+﻿
+namespace fasttask
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.readTaskBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.appWorkPath = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textCommand = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.threatCount = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.taskcount = new System.Windows.Forms.TextBox();
+            this.outputTextBox = new System.Windows.Forms.RichTextBox();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.tBoxTask = new System.Windows.Forms.RichTextBox();
+            this.textBoxjindu = new System.Windows.Forms.RichTextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // readTaskBtn
+            // 
+            this.readTaskBtn.Location = new System.Drawing.Point(200, 31);
+            this.readTaskBtn.Name = "readTaskBtn";
+            this.readTaskBtn.Size = new System.Drawing.Size(75, 23);
+            this.readTaskBtn.TabIndex = 0;
+            this.readTaskBtn.Text = "读取任务";
+            this.readTaskBtn.UseVisualStyleBackColor = true;
+            this.readTaskBtn.Click += new System.EventHandler(this.readTaskBtn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "任务列表：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(291, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "程序目录：";
+            // 
+            // appWorkPath
+            // 
+            this.appWorkPath.ForeColor = System.Drawing.Color.Gray;
+            this.appWorkPath.Location = new System.Drawing.Point(362, 71);
+            this.appWorkPath.Name = "appWorkPath";
+            this.appWorkPath.Size = new System.Drawing.Size(291, 21);
+            this.appWorkPath.TabIndex = 4;
+            this.appWorkPath.Text = "常用命令无需配置";
+            this.appWorkPath.Enter += new System.EventHandler(this.appWorkPath_Enter);
+            this.appWorkPath.Leave += new System.EventHandler(this.appWorkPath_Leave);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(291, 119);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 12);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "命令参数：";
+            // 
+            // textCommand
+            // 
+            this.textCommand.ForeColor = System.Drawing.Color.Gray;
+            this.textCommand.Location = new System.Drawing.Point(362, 116);
+            this.textCommand.Name = "textCommand";
+            this.textCommand.Size = new System.Drawing.Size(291, 21);
+            this.textCommand.TabIndex = 6;
+            this.textCommand.Text = "栗子：ping *r > *f.txt";
+            this.textCommand.Enter += new System.EventHandler(this.textCommand_Enter);
+            this.textCommand.Leave += new System.EventHandler(this.textCommand_Leave);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(291, 157);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 12);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "线程数量：";
+            // 
+            // threatCount
+            // 
+            this.threatCount.Location = new System.Drawing.Point(362, 157);
+            this.threatCount.Name = "threatCount";
+            this.threatCount.Size = new System.Drawing.Size(100, 21);
+            this.threatCount.TabIndex = 8;
+            this.threatCount.Text = "10";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(33, 276);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(65, 12);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "消息通知：";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(293, 235);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 11;
+            this.btnStart.Text = "开始任务";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(291, 30);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(65, 12);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "任务数量：";
+            // 
+            // taskcount
+            // 
+            this.taskcount.Location = new System.Drawing.Point(362, 27);
+            this.taskcount.Name = "taskcount";
+            this.taskcount.Size = new System.Drawing.Size(100, 21);
+            this.taskcount.TabIndex = 13;
+            this.taskcount.Text = "0";
+            // 
+            // outputTextBox
+            // 
+            this.outputTextBox.BackColor = System.Drawing.Color.Black;
+            this.outputTextBox.Location = new System.Drawing.Point(35, 301);
+            this.outputTextBox.Name = "outputTextBox";
+            this.outputTextBox.Size = new System.Drawing.Size(333, 170);
+            this.outputTextBox.TabIndex = 15;
+            this.outputTextBox.Text = "";
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(423, 235);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 16;
+            this.btnStop.Text = "结束任务";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tBoxTask
+            // 
+            this.tBoxTask.ForeColor = System.Drawing.Color.Gray;
+            this.tBoxTask.Location = new System.Drawing.Point(35, 60);
+            this.tBoxTask.Name = "tBoxTask";
+            this.tBoxTask.Size = new System.Drawing.Size(240, 198);
+            this.tBoxTask.TabIndex = 17;
+            this.tBoxTask.Text = "将文件拖入程序即可导入任务";
+            this.tBoxTask.Enter += new System.EventHandler(this.tBoxTask_Enter);
+            this.tBoxTask.Leave += new System.EventHandler(this.tBoxTask_Leave);
+            // 
+            // textBoxjindu
+            // 
+            this.textBoxjindu.BackColor = System.Drawing.Color.Black;
+            this.textBoxjindu.Location = new System.Drawing.Point(423, 301);
+            this.textBoxjindu.Name = "textBoxjindu";
+            this.textBoxjindu.Size = new System.Drawing.Size(333, 170);
+            this.textBoxjindu.TabIndex = 18;
+            this.textBoxjindu.Text = "";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(421, 276);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(65, 12);
+            this.label7.TabIndex = 19;
+            this.label7.Text = "进度通知：";
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 473);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBoxjindu);
+            this.Controls.Add(this.tBoxTask);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.outputTextBox);
+            this.Controls.Add(this.taskcount);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.threatCount);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textCommand);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.appWorkPath);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.readTaskBtn);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Name = "Form1";
+            this.Text = "本地多任务并发工具";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.Form1_DragEnter);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button readTaskBtn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox appWorkPath;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textCommand;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox threatCount;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox taskcount;
+        private System.Windows.Forms.RichTextBox outputTextBox;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.RichTextBox tBoxTask;
+        private System.Windows.Forms.RichTextBox textBoxjindu;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ fasttask/Form1.cs
@@ -0,0 +1,293 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace fasttask
+{
+    public partial class Form1 : Form
+    {
+        private List<string> targets;
+        private int threadCount;
+        private List<Process> runningProcesses;
+        public Form1()
+        {
+            InitializeComponent();
+            runningProcesses = new List<Process>();
+        }
+
+        private List<string> ReadTargetsFromFile(string filePath)
+        {
+            // 从txt文件逐行读取任务目标
+            List<string> targets = new List<string>();
+            tBoxTask.Text = "";
+            using (StreamReader reader = new StreamReader(filePath))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    targets.Add(line);
+                    tBoxTask.AppendText(line + Environment.NewLine);
+                }
+            }
+            return targets;
+        }
+
+        private void Form1_DragEnter(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            if (files.Length > 0)
+            {
+                if (tBoxTask.Text == "将文件拖入程序即可导入任务")
+                {
+                    tBoxTask.Text = "";
+                    tBoxTask.ForeColor = Color.Black;
+                }
+                targets = ReadTargetsFromFile(files[0]);
+                taskcount.Text = targets.Count.ToString();
+
+            }
+        }
+
+        private void readTaskBtn_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Text Files (*.txt)|*.txt";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                // 从txt文件逐行读取任务目标
+                targets = ReadTargetsFromFile(openFileDialog.FileName);
+
+                //  显示任务目标数量
+                taskcount.Text = targets.Count.ToString();
+            }
+        }
+
+        private async void btnStart_Click(object sender, EventArgs e)
+        {
+            AppendColoredText(appWorkPath.Text, Color.Red);
+            AppendColoredText("[INFO]开始任务，本次任务总数:" + threadCount.ToString(), Color.Red);
+            threadCount = Convert.ToInt32(threatCount.Text);
+            Queue<string> taskQueue = new Queue<string>(targets);
+            object taskLock = new object();
+            int completedTasks = 0;
+
+            List<Task> tasks = new List<Task>();
+
+            for (int i = 0; i < threadCount; i++)
+            {
+                tasks.Add(RunTaskAsync(taskQueue, taskLock));
+            }
+
+            await Task.WhenAll(tasks);
+
+            outputTextBox.Invoke((MethodInvoker)delegate
+            {
+                AppendColoredText("[INFO]所有任务已完成。", Color.Red);
+            });
+        }
+
+        private async Task RunTaskAsync(Queue<string> taskQueue, object taskLock)
+        {
+            while (true)
+            {
+                string target;
+                lock (taskLock)
+                {
+                    if (taskQueue.Count == 0)
+                    {
+                        break;
+                    }
+                    target = taskQueue.Dequeue();
+                }
+                string command = textCommand.Text.Replace("*r", target);
+                string filename = target.Replace("/", "_").Replace(@"\", "_").Replace(":","_").Replace("*", "_").Replace('"', '_').Replace(">", "_").Replace("<", "_").Replace("|", "_");
+                command = command.Replace("*f", filename);
+                AppendColoredText("[DEBUG]当前任务:" + command, Color.White);
+                
+                Process process = new Process();
+                
+                if (appWorkPath.Text != "常用命令无需配置" && appWorkPath.Text != "")
+                {
+                    process.StartInfo.WorkingDirectory = appWorkPath.Text;
+                }
+                process.StartInfo.FileName = "cmd.exe";
+                process.StartInfo.Arguments = "/c " + command;
+                process.StartInfo.RedirectStandardOutput = true;
+                process.StartInfo.UseShellExecute = false;
+                process.StartInfo.CreateNoWindow = true;
+                process.OutputDataReceived += new DataReceivedEventHandler(OutputHandler);
+                process.Start();
+                process.BeginOutputReadLine();
+                await WaitForExitAsync(process);
+                AppendColoredText2("[INFO]任务完成:" + target, Color.White);
+            }
+        }
+
+        private Task<int> WaitForExitAsync(Process process)
+        {
+            var tcs = new TaskCompletionSource<int>();
+            process.EnableRaisingEvents = true;
+            process.Exited += (sender, args) => tcs.TrySetResult(process.ExitCode);
+            if (process.HasExited)
+            {
+                tcs.TrySetResult(process.ExitCode);
+            }
+            return tcs.Task;
+        }
+
+
+
+        private void OutputHandler(object sender, DataReceivedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(e.Data))
+            {
+                string output = e.Data;
+                // 使用Invoke从后台线程更新UI控件
+                outputTextBox.Invoke((MethodInvoker)delegate
+                {
+                    AppendColoredText("[INFO]"+ output, Color.Green);
+                });
+            }
+        }
+
+        private void AppendColoredText(string text, Color color)
+        {
+            // 追加消息通知
+
+            outputTextBox.Invoke((MethodInvoker)delegate
+            {
+                outputTextBox.SelectionStart = outputTextBox.TextLength;
+                outputTextBox.SelectionLength = 0;
+                outputTextBox.SelectionColor = color;
+                outputTextBox.AppendText(text + Environment.NewLine);
+                outputTextBox.SelectionColor = outputTextBox.ForeColor;
+                // 滚动到插入符号位置
+                outputTextBox.ScrollToCaret();
+            });
+        }
+
+        private void AppendColoredText2(string text, Color color)
+        {
+            // 追加进度通知
+
+            textBoxjindu.Invoke((MethodInvoker)delegate
+            {
+                textBoxjindu.SelectionStart = outputTextBox.TextLength;
+                textBoxjindu.SelectionLength = 0;
+                textBoxjindu.SelectionColor = color;
+                textBoxjindu.AppendText(text + Environment.NewLine);
+                textBoxjindu.SelectionColor = outputTextBox.ForeColor;
+                // 滚动到插入符号位置
+                textBoxjindu.ScrollToCaret();
+            });
+        }
+
+        private void Form1_DragDrop(object sender, DragEventArgs e)
+        {
+            // 拖入增加任务
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            if (files.Length > 0)
+            {
+                targets = ReadTargetsFromFile(files[0]);
+                taskcount.Text = targets.Count.ToString();
+                if (tBoxTask.Text == "将文件拖入程序即可导入任务")
+                {
+                    tBoxTask.Text = "";
+                    tBoxTask.ForeColor = Color.Black;
+                }
+
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            // 窗体关闭时清理线程
+            foreach (Process process in runningProcesses)
+            {
+                process.Kill();
+            }
+            runningProcesses.Clear();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // 关闭线程
+            foreach (Process process in runningProcesses)
+            {
+                process.Kill();
+            }
+            runningProcesses.Clear();
+            AppendColoredText("[INFO]任务已清空。", Color.Red);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // 设置线程数量
+            threadCount = 10;
+
+            // 初始化任务列表
+            targets = new List<string>();
+
+            AppendColoredText("[INFO]欢迎使用本地多任务并发工具\r\n[INFO]将文件拖入程序即可导入任务", Color.Green);
+        }
+
+        private void appWorkPath_Enter(object sender, EventArgs e)
+        {
+            if (appWorkPath.Text == "常用命令无需配置")
+            {
+                appWorkPath.Text = "";
+                appWorkPath.ForeColor = Color.Black;
+            }
+        }
+
+        private void appWorkPath_Leave(object sender, EventArgs e)
+        {
+            if (appWorkPath.Text == "")
+            {
+                appWorkPath.Text = "常用命令无需配置";
+                appWorkPath.ForeColor = Color.Gray;
+            }
+        }
+
+        private void textCommand_Enter(object sender, EventArgs e)
+        {
+            if (textCommand.Text == "栗子：ping *r > *f.txt")
+            {
+                textCommand.Text = "";
+                textCommand.ForeColor = Color.Black;
+            }
+        }
+
+        private void textCommand_Leave(object sender, EventArgs e)
+        {
+            if (textCommand.Text == "")
+            {
+                textCommand.Text = "栗子：ping *r > *f.txt";
+                textCommand.ForeColor = Color.Gray;
+            }
+        }
+
+        private void tBoxTask_Enter(object sender, EventArgs e)
+        {
+            if (tBoxTask.Text == "将文件拖入程序即可导入任务")
+            {
+                tBoxTask.Text = "";
+                tBoxTask.ForeColor = Color.Black;
+            }
+        }
+
+        private void tBoxTask_Leave(object sender, EventArgs e)
+        {
+            if (tBoxTask.Text == "")
+            {
+                tBoxTask.Text = "将文件拖入程序即可导入任务";
+                tBoxTask.ForeColor = Color.Gray;
+            }
+        }
+    }
+}
